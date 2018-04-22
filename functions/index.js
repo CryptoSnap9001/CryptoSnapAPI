ccfe2bdb541da7e293cb7fa7362cdd2d79d6283c
Menachem Hornbacher <mendelh1537@gmail.com>
2018-04-22 02:46:36 -0400

    let's see if this works

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,15 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// StellarSdk is required to connect to the Stellar blockchain network
+var StellarSdk = require('stellar-sdk');
+
+exports.createStellarSecret = functions.database.ref( "/users/{ user_id }" )
+    .onWrite( (snapshot, context) => {
+        console.log( snapshot, context );
+    });
