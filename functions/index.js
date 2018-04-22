e1e6021f5c0b45ca376b02cec3773df4eb78159c
Menachem Hornbacher <mendelh1537@gmail.com>
2018-04-22 04:02:41 -0400

    create a stellar account whenever a new user is entered in the DBS

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-
+var request = require('request');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -14,7 +14,45 @@ admin.initializeApp();
 // StellarSdk is required to connect to the Stellar blockchain network
 var StellarSdk = require('stellar-sdk');
 
-exports.createStellarSecret = functions.database.ref( "/users/{ user_id }" )
-    .onWrite( (snapshot, context) => {
-        console.log( snapshot, context );
+/**
+ * createStellarAccount
+ * 
+ * Submits a GET request to create a Stellar account for the provided Keypair
+ * 
+ * @param {StellarSdk.Keypair} pair 
+ */
+const createStellarAccount = ( pair ) => {
+    request.get({
+        url: 'https://friendbot.stellar.org',
+        qs: { addr: pair.publicKey() },
+        json: true
+    }, function(error, response, body) {
+        if (error || response.statusCode !== 200) {
+          console.error('Stellar account Error', error || body);
+        }
+        else {
+          console.log('Stellar account success :)\n', body);
+        }
+    });
+}
+
+/**
+ * createStellarSecret
+ * 
+ * Generate a crypto wallet for each user as it joins the network
+ */
+exports.createStellarSecret = functions.database.ref( "/users/{user_id}" )
+    .onCreate( (snapshot, context) => {
+        const user_id = context.params.user_id;
+        // generate the keys that we need
+        var pair = StellarSdk.Keypair.random(); // generate the key
+        // create a wallet for them in the background
+        console.log( `Creating stellar account for ${user_id}` );
+        createStellarAccount( pair );
+        // update the database with the encryption secrets
+        return admin.database().ref( `/users/${user_id}` )
+            .update( { 
+                secret: pair.secret(),
+                public_key: pair.publicKey()
+            });
     });
